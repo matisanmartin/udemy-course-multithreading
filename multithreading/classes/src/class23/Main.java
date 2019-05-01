719a6aaa60f1142adc01ea078797cf637f08d78b
Matias San Martin Acosta <msanmartinacosta@gmail.com>
2019-05-01 19:24:26 -0300

    Agrega clases

---
+++ multithreading/classes/src/class23/Main.java
@@ -0,0 +1,114 @@
+package class23;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    private static final int N = 10;
+
+    private static class MatricesMultiplierConsumer extends Thread {
+        private ThreadSafeQueue queue;
+        private FileWriter fileWriter;
+
+        public MatricesMultiplierConsumer(FileWriter fileWriter, ThreadSafeQueue queue) {
+            this.fileWriter = fileWriter;
+            this.queue = queue;
+        }
+    }
+
+    private static class MatricesReaderProducer extends Thread {
+        private Scanner scanner;
+        private ThreadSafeQueue queue;
+
+        public MatricesReaderProducer(FileReader reader, ThreadSafeQueue queue) {
+            this.scanner = new Scanner(reader);
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
+            while(true) {
+                float[][] matrix1 = readMatrix();
+                float[][] matrix2 = readMatrix();
+                if(matrix1 == null || matrix2 == null) {
+                    queue.terminate();
+                    System.out.println("No more matrices. Producer thread terminating");
+                    return;
+                }
+
+                MatricesPair matricesPair= new MatricesPair();
+                matricesPair.matrix1 = matrix1;
+                matricesPair.matrix2 = matrix2;
+
+                queue.add(matricesPair);
+
+            }
+        }
+
+        private float[][] readMatrix() {
+
+            float[][] matrix = new float[N][N];
+
+            for(int r = 0; r < N; r++) {
+                if(!scanner.hasNext())
+                    return null;
+
+                String[] line = scanner.nextLine().split(",");
+                for(int c= 0; c <N; c++) {
+                    matrix[r][c] = Float.valueOf(line[c]);
+                }
+            }
+            scanner.nextLine();
+            return matrix;
+        }
+    }
+    private static class ThreadSafeQueue {
+        private Queue<MatricesPair> queue = new LinkedList<>();
+        private boolean isEmpty = true;
+        private boolean isTerminate = false;
+
+        public synchronized void add(MatricesPair matricesPair) {
+            queue.add(matricesPair);
+            isEmpty = false;
+            notify();
+        }
+
+        public synchronized  MatricesPair remove() {
+            while(isEmpty && !isTerminate) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+
+                }
+            }
+
+            if(queue.size() == 1) {
+                isEmpty = true;
+            }
+
+            if(queue.size() == 0 && isTerminate) {
+                return null;
+            }
+
+            System.out.println("queue size " + queue.size());
+
+            return queue.remove();
+
+        }
+
+        public synchronized void terminate() {
+            isTerminate = true;
+            notifyAll();
+
+        }
+    }
+
+
+    private static class MatricesPair {
+        public float[][] matrix1;
+        public float[][] matrix2;
+    }
+}
